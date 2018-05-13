5d248b333dccf323360810c47a2e1457dd09253e
Martijn van der Ven <martijn@vanderven.se>
2018-05-13 14:48:26 +0200

    Move constraints, remove COMMIT
    No need to COMMIT at the end of the SQL file. Move the PRIMARY KEY
    constraint to the location for a column constraint for maximum
    compatibility. Take away the indentation on the setting constraint, as
    it is really a table and not a column constraint.

--- schema.sql
+++ schema.sql
@@ -1,16 +1,14 @@
 CREATE TABLE tokens (
-    token_id       CHARACTER(64)                                    NOT NULL UNIQUE,
+    token_id       CHARACTER(64)                                    NOT NULL PRIMARY KEY,
     token_hash     CHARACTER VARYING(255)                           NOT NULL,
     auth_me        CHARACTER VARYING(255)                           NOT NULL,
     auth_client_id CHARACTER VARYING(255)                           NOT NULL,
     auth_scope     CHARACTER VARYING(255)                           NOT NULL,
     created        TIMESTAMP              DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    revoked        TIMESTAMP              DEFAULT NULL,
-    PRIMARY KEY (token_id)
+    revoked        TIMESTAMP              DEFAULT NULL
 );
 CREATE TABLE settings (
     setting_name   CHARACTER VARYING(255) NOT NULL,
     setting_value  CHARACTER VARYING(255) NOT NULL,
-                   CONSTRAINT setting UNIQUE (setting_name, setting_value)
+    CONSTRAINT setting UNIQUE (setting_name, setting_value)
 );
-COMMIT;
