78517f671fdb4f30354380f803630c2b10b84756
Martijn van der Ven <martijn@vanderven.se>
2018-05-13 15:06:30 +0200

    Space in data type was giving issues
    Some tools really do not like the space in CHARACTER VARYING. Switch to
    CHARVAR instead. Also switch to CHAR to match.
    
    Add a lint tool for the SQL file.

--- schema.sql
+++ schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE tokens (
-    token_id       CHARACTER(64)                                    NOT NULL PRIMARY KEY,
-    token_hash     CHARACTER VARYING(255)                           NOT NULL,
-    auth_me        CHARACTER VARYING(255)                           NOT NULL,
-    auth_client_id CHARACTER VARYING(255)                           NOT NULL,
-    auth_scope     CHARACTER VARYING(255)                           NOT NULL,
-    created        TIMESTAMP              DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    revoked        TIMESTAMP              DEFAULT NULL
+    token_id       CHAR(64)                               NOT NULL PRIMARY KEY,
+    token_hash     VARCHAR(255)                           NOT NULL,
+    auth_me        VARCHAR(255)                           NOT NULL,
+    auth_client_id VARCHAR(255)                           NOT NULL,
+    auth_scope     VARCHAR(255)                           NOT NULL,
+    created        TIMESTAMP    DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    revoked        TIMESTAMP    DEFAULT NULL
 );
 CREATE TABLE settings (
-    setting_name   CHARACTER VARYING(255) NOT NULL,
-    setting_value  CHARACTER VARYING(255) NOT NULL,
+    setting_name   VARCHAR(255) NOT NULL,
+    setting_value  VARCHAR(255) NOT NULL,
     CONSTRAINT setting UNIQUE (setting_name, setting_value)
 );
