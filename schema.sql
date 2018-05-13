ae5998327e02c5700c2040993387e8564de7b395
Martijn van der Ven <martijn@vanderven.se>
2018-05-13 14:28:35 +0200

    Do not use reserved words for column names

--- schema.sql
+++ schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE tokens (
     PRIMARY KEY (token_id),
-    token_id   CHARACTER(64)          NOT NULL UNIQUE,
-    token_hash CHARACTER VARYING(255) NOT NULL,
-    me         CHARACTER VARYING(255) NOT NULL,
-    client_id  CHARACTER VARYING(255) NOT NULL,
-    scope      CHARACTER VARYING(255) NOT NULL,
-    created    TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    revoked    TIMESTAMP                       DEFAULT NULL
+    token_id       CHARACTER(64)          NOT NULL UNIQUE,
+    token_hash     CHARACTER VARYING(255) NOT NULL,
+    auth_me        CHARACTER VARYING(255) NOT NULL,
+    auth_client_id CHARACTER VARYING(255) NOT NULL,
+    auth_scope     CHARACTER VARYING(255) NOT NULL,
+    created        TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    revoked        TIMESTAMP                       DEFAULT NULL
 );
 CREATE TABLE settings (
-    name  CHARACTER VARYING(255) NOT NULL,
-    value CHARACTER VARYING(255) NOT NULL,
-          CONSTRAINT setting UNIQUE(name, value)
+    setting_name   CHARACTER VARYING(255) NOT NULL,
+    setting_value  CHARACTER VARYING(255) NOT NULL,
+                   CONSTRAINT setting UNIQUE (setting_name, setting_value)
 );
 COMMIT;
