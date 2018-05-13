5d61cf16bd2c17b8cbea82fb7f488833e97a7cb0
Martijn van der Ven <martijn@vanderven.se>
2018-05-13 14:28:53 +0200

    Move primary key down again, because SQLite

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE tokens (
-    PRIMARY KEY (token_id),
     token_id       CHARACTER(64)          NOT NULL UNIQUE,
     token_hash     CHARACTER VARYING(255) NOT NULL,
     auth_me        CHARACTER VARYING(255) NOT NULL,
     auth_client_id CHARACTER VARYING(255) NOT NULL,
     auth_scope     CHARACTER VARYING(255) NOT NULL,
     created        TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    revoked        TIMESTAMP                       DEFAULT NULL
+    revoked        TIMESTAMP                       DEFAULT NULL,
+    PRIMARY KEY (token_id)
 );
 CREATE TABLE settings (
     setting_name   CHARACTER VARYING(255) NOT NULL,
