c2fc47a6de23af5ce51bf04c01f4f0a43db2be4d
Martijn van der Ven <martijn@vanderven.se>
2018-05-13 12:39:58 +0200

    Define primary key first, per style guide

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE tokens (
+    PRIMARY KEY (token_id),
     token_id   CHARACTER(64)          NOT NULL UNIQUE,
     token_hash CHARACTER VARYING(255) NOT NULL,
     me         CHARACTER VARYING(255) NOT NULL,
     client_id  CHARACTER VARYING(255) NOT NULL,
     scope      CHARACTER VARYING(255) NOT NULL,
     created    TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    revoked    TIMESTAMP                       DEFAULT NULL,
-    PRIMARY KEY(token_id)
+    revoked    TIMESTAMP                       DEFAULT NULL
 );
 CREATE TABLE settings (
     name  CHARACTER VARYING(255) NOT NULL,
