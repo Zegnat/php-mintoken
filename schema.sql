1969ce2e90c689c4a5a11db87af038024a976f6b
Martijn van der Ven <martijn@vanderven.se>
2018-05-13 11:15:10 +0200

    Remove quotes

--- schema.sql
+++ schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE `tokens` (
-	`token_id`	TEXT NOT NULL UNIQUE,
-	`token_hash`	TEXT NOT NULL,
-	`me`	TEXT NOT NULL,
-	`client_id`	TEXT NOT NULL,
-	`scope`	TEXT NOT NULL,
-	`created`	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`revoked`	TEXT DEFAULT NULL,
-	PRIMARY KEY(`token_id`)
+CREATE TABLE tokens (
+	token_id	TEXT NOT NULL UNIQUE,
+	token_hash	TEXT NOT NULL,
+	me	TEXT NOT NULL,
+	client_id	TEXT NOT NULL,
+	scope	TEXT NOT NULL,
+	created	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	revoked	TEXT DEFAULT NULL,
+	PRIMARY KEY(token_id)
 );
-CREATE TABLE `settings` (
-	`name`	TEXT NOT NULL,
-	`value`	TEXT NOT NULL,
-	CONSTRAINT `setting` UNIQUE(`name`,`value`)
+CREATE TABLE settings (
+	name	TEXT NOT NULL,
+	value	TEXT NOT NULL,
+	CONSTRAINT setting UNIQUE(name, value)
 );
 COMMIT;
