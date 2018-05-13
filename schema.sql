04748e63f0e9feee2bfbd5ddc076c5137e2ee36d
Martijn van der Ven <martijn@vanderven.se>
2018-05-13 14:35:40 +0200

    Per SQL-92 BNF, default clause comes first

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE tokens (
-    token_id       CHARACTER(64)          NOT NULL UNIQUE,
-    token_hash     CHARACTER VARYING(255) NOT NULL,
-    auth_me        CHARACTER VARYING(255) NOT NULL,
-    auth_client_id CHARACTER VARYING(255) NOT NULL,
-    auth_scope     CHARACTER VARYING(255) NOT NULL,
-    created        TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    revoked        TIMESTAMP                       DEFAULT NULL,
+    token_id       CHARACTER(64)                                    NOT NULL UNIQUE,
+    token_hash     CHARACTER VARYING(255)                           NOT NULL,
+    auth_me        CHARACTER VARYING(255)                           NOT NULL,
+    auth_client_id CHARACTER VARYING(255)                           NOT NULL,
+    auth_scope     CHARACTER VARYING(255)                           NOT NULL,
+    created        TIMESTAMP              DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    revoked        TIMESTAMP              DEFAULT NULL,
     PRIMARY KEY (token_id)
 );
 CREATE TABLE settings (
